a9cb0c67108d09f685b867ff6d2851b06b9a4fee
s34rching <ivan.katsenya@gmail.com>
2019-03-18 17:27:09 +0300

    mathers

--- calculator.spec.js
+++ calculator.spec.js
@@ -30,4 +30,56 @@ describe('calculator.js', function() {
     expect(calculator.total).toBe(1);
   });
 
+  it('should not divide number by zero', function() {
+    const calculator = new Calculator();
+    calculator.total = 50;
+
+    expect(function() {calculator.divide(0)}).toThrow();
+    expect(function() {calculator.divide(0)}).toThrowError(Error);
+    expect(function() {calculator.divide(0)}).toThrowError(Error, 'Cannot divide by 0');
+  });
+
+  it('can overwrite total', function() {
+    const calculator = new Calculator();
+    calculator.total = null;
+
+    expect(calculator.total).toBeNull();
+  });
+
+  // Undefined (== primitive) - a value that has been just declared (not an object and doesn't have methods)
+  it('has common operations', function() {
+    const calculator = new Calculator();
+
+    expect(calculator.add).toBeDefined();
+  })
+
+  it('does not handle NaN', function() {
+    const calculator = new Calculator();
+    calculator.total = 50;
+    calculator.multiply('a');
+
+    expect(calculator.total).toBeNaN();
+  });
+
+// toMatch() - applicable for strings and regex
+  it('returns total as a number', function() {
+    const calculator = new Calculator();
+    calculator.total = 50;
+    calculator.add(4);
+
+    expect(calculator.total).toMatch(/-?\d+/);
+    expect(typeof(calculator.total)).toMatch('number');
+  });
+
+// if the result is not obvious
+  it('attempt to try asymmetric matchers', function() {
+    const calculator = new Calculator();
+    calculator.total = 600 + Math.random()*50;
+    calculator.add(4);
+
+    expect(calculator.total).toEqual(jasmine.anything());
+    //expect(null).not.toEqual(jasmine.anything());
+    //expect(undefined).not.toEqual(jasmine.anything());
+  });
+
 });
