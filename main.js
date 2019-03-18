67b3f5f2e2f0c94d97cc1b4fd0b9bcaf0d73d8f6
s34rching <ivan.katsenya@gmail.com>
2019-03-18 12:32:51 +0300

    created simple calculator

---
+++ main.js
@@ -0,0 +1,34 @@
+function calculate(inputValue) {
+  const expression = /\+|\-|\*|\//;
+  const operation = inputValue.match(expression);
+
+  const numbers = inputValue.split(operation);
+  const numberA = parseInt(numbers[0]);
+  const numberB = parseInt(numbers[1]);
+
+  let result;
+  const calculator = new Calculator();
+  calculator.add(numberA);
+
+  switch (operation[0]) {
+    case '+': result = calculator.add(numberB);
+      break;
+    case '-': result = calculator.subtract(numberB);
+      break;
+    case '*': result = calculator.multiply(numberB);
+      break;
+    case '/': result = calculator.divide(numberB);
+      break;
+  }
+
+  updateResult(result);
+  //document.getElementById('result').innerHTML = result;
+};
+
+function updateResult(result) {
+  const element = document.getElementById('result');
+
+  if (element) {
+    element.innerText = result;
+  }
+};
